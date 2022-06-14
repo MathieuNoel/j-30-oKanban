3e6c548bd85efb6d74467c4a0bc077a95c79f8b0
Mathieu Noel <yzorien@gmail.com>
2022-06-14 22:58:44 +0200

    done

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -4,7 +4,7 @@ const {List} = require('../models');
 const mainController = {
   homePage: async (req, res) => {
     try {
-      const lists = await List.findAll()      
+      const lists = await List.findAll({include: {association: 'card', include: 'labels'}})      
      console.log('LALALA',lists)
      res.render('home',{lists})      
     } catch (error) {
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = require('./app/router');
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.use(express.static('./public')); 
+app.use(express.static('public')); 
 
 app.use(router);
 
